306d24fc2229424a7609bd818d6493a862514c2d
Omar Ali Sheikh <sheikhomar@users.noreply.github.com>
2022-11-06 13:05:33 +0100

    Note about the annotator port issue.

--- scripts/zenml/register-label-studio.sh
+++ scripts/zenml/register-label-studio.sh
@@ -13,6 +13,10 @@ export LABEL_STUDIO_HOST_NAME=$(get_terraform_output label_studio_ingress_host)
 echo " - Retrieving Label Studio API key..."
 export LABEL_STUDIO_API_KEY=$(get_terraform_output label_studio_default_user_token)
 
+# Workaround: ZenML fails to register annotator if the port
+# specificed with the --port option is not available in the
+# local machine, even though the annotator is running on
+# a remove server.
 if [ -z "$LABEL_STUDIO_PORT" ]; then
     export LABEL_STUDIO_PORT=$(python -c 'import socket; s=socket.socket(); s.bind(("", 0)); print(s.getsockname()[1]); s.close()')
 fi
