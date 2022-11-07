43a7218399276029806bf8837299f0602cb67b46
Omar Ali Sheikh <sheikhomar@users.noreply.github.com>
2022-11-07 06:24:33 +0100

    Register annotator with SSL port.
    Notice that this only works once a patch is applied:
    https://github.com/zenml-io/zenml/pull/1045/files

--- scripts/zenml/register-label-studio.sh
+++ scripts/zenml/register-label-studio.sh
@@ -13,14 +13,6 @@ export LABEL_STUDIO_HOST_NAME=$(get_terraform_output label_studio_ingress_host)
 echo " - Retrieving Label Studio API key..."
 export LABEL_STUDIO_API_KEY=$(get_terraform_output label_studio_default_user_token)
 
-# Workaround: ZenML fails to register annotator if the port
-# specificed with the --port option is not available in the
-# local machine, even though the annotator is running on
-# a remove server.
-if [ -z "$LABEL_STUDIO_PORT" ]; then
-    export LABEL_STUDIO_PORT=$(python -c 'import socket; s=socket.socket(); s.bind(("", 0)); print(s.getsockname()[1]); s.close()')
-fi
-
 echo " - Storing Label Studio API key in the secret: '$ZENML_LABEL_STUDIO_SECRET_NAME' ..."
 poetry run zenml secrets-manager secret register $ZENML_LABEL_STUDIO_SECRET_NAME \
     --api_key="$LABEL_STUDIO_API_KEY"
@@ -30,7 +22,7 @@ poetry run zenml annotator register $ZENML_LABEL_STUDIO_NAME \
     --flavor label_studio \
     --authentication_secret=$ZENML_LABEL_STUDIO_SECRET_NAME \
     --instance_url="https://$LABEL_STUDIO_HOST_NAME" \
-    --port=$LABEL_STUDIO_PORT
+    --port=443
 
 echo " - Updating the stack with the annotator..."
 poetry run zenml stack update $ZENML_STACK_NAME \
